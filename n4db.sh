3d3cf5569618962cbba9bb8564858c1bb4471b1c
N4ND4 <47235064+Nanda-N4@users.noreply.github.com>
2022-12-23 16:40:50 +0630

    Update n4db.sh

--- n4db.sh
+++ n4db.sh
@@ -20,7 +20,9 @@ echo ""
 echo "Bat dau cai dat Dropbear"
 echo ""
 apt-get -y install dropbear ssh
-curl https://raw.githubusercontent.com/khaiphan9x/auto-installer-dropbear/master/dropbear > /etc/default/dropbear
+sed -i 's/NO_START\=1/NO_START\=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT\=22/DROPBEAR_PORT\=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS\=/DROPBEAR_EXTRA_ARGS\=\"\-p 443 \-p 80 \-p 8080 \-p 8484 \-p 143 \-p 109\"/g' /etc/default/dropbear
 sed -i 's/#PermitRootLogin prohibit\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin without\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
@@ -30,7 +32,7 @@ service sshd restart
 echo ""
 echo ""
 echo "Da hoan thanh cai dat Dropbear"
-echo "Dropbear dang chay tren port: 443, 80"
+echo "Dropbear dang chay tren port: 442, 80, 8080, 8484, 143, 109"
 echo "Hay tao 1 user moi de su dung"
 echo ""
 echo "Dang khoi dong lai server..."
